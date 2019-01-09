2e07e74b1d9351d58d7e80c0e890948c1407fa3f
Jo Lingenfelter <jo@ns804.com>
2019-01-09 18:01:40 -0500

    basic project setup

---
+++ Combined_Beacon_And_Finder/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Combined_Beacon_And_Finder
+//
+//  Created by Jo Lingenfelter on 1/9/19.
+//  Copyright © 2019 Jo Lingenfelter. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
