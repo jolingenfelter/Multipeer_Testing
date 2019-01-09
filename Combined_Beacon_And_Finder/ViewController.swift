7e763484b98d7dc0fc85bdf7c51ac093299a0581
Jo Lingenfelter <jo@ns804.com>
2019-01-09 17:46:37 -0500

    Initial Commit

---
+++ Combined_Beacon_And_Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Combined_Beacon_And_Finder
+//
+//  Created by Jo Lingenfelter on 1/9/19.
+//  Copyright © 2019 Jo Lingenfelter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Combined_Beacon_And_FinderTests/Combined_Beacon_And_FinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Combined_Beacon_And_FinderTests.swift
+//  Combined_Beacon_And_FinderTests
+//
+//  Created by Jo Lingenfelter on 1/9/19.
+//  Copyright © 2019 Jo Lingenfelter. All rights reserved.
+//
+
+import XCTest
+@testable import Combined_Beacon_And_Finder
+
+class Combined_Beacon_And_FinderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
